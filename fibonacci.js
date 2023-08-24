464e73b4f340a744fc9f47b9d6df6d64fbf1668a
djfor5 <117873331+djfor5@users.noreply.github.com>
2023-08-24 14:07:27 +1000

    Rename functions

--- fibonacci.js
+++ fibonacci.js
@@ -1,4 +1,4 @@
-function fibs(n) {
+function fibsI(n) {
   let arr = [];
   if (n === 0) {
     arr = [0];
@@ -16,19 +16,19 @@ function fibs(n) {
   return arr;
 }
 
-function fibsRec(n) {
+function fibsR(n) {
   if (n === 0) return [0];
   if (n === 1) return [0, 1];
 
-  return fibsRec(n - 1).concat(Number(fibsRec(n - 1).slice(-2, -1)) + Number(fibsRec(n - 1).slice(-1))); // eslint-disable-line max-len
+  return fibsR(n - 1).concat(Number(fibsR(n - 1).slice(-2, -1)) + Number(fibsR(n - 1).slice(-1)));
 }
 
 const n = 15;
 
 console.time();
-console.log(fibs(n));
+console.log(fibsI(n));
 console.timeEnd();
 
 console.time();
-console.log(fibsRec(n));
+console.log(fibsR(n));
 console.timeEnd();
